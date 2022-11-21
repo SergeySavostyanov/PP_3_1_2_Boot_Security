026433924c50a606ffd14743560315fbf0282cd2
Sergei <395105@mail.ru>
2022-11-21 19:10:37 +0400

    - security

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/HelloController.java
@@ -0,0 +1,36 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.kata.spring.boot_security.demo.security.PersonDetails;
+import ru.kata.spring.boot_security.demo.services.AdminService;
+
+@Controller
+public class HelloController {
+    private final AdminService adminService;
+
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello";
+    }
+
+
+    @GetMapping ("/showUserInfo")
+    public String showUserInfo(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+        return "hello";
+    }
+    @GetMapping("/admin")
+    public String adminPageP(){
+        adminService.doAdminStuff();
+        return "admin";
+    }
+}
